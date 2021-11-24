8fc2a2e108b0dc144a4c337972f12cc08aa46e91
vlagh3 <82247523+vlagh3@users.noreply.github.com>
2021-11-25 00:16:44 +0200

    comm cleanup + notify & exit if decryption of vault fails

--- helpers/obsdstart.sh
+++ helpers/obsdstart.sh
@@ -6,14 +6,14 @@
 # Mount/Umount your encrypted obsidian
 # vault on the go. A helper script
 #
-# NOTE: This script is called as a keybind by i3 
-# (see ~/.config/i3/config file)
+# NOTE: This script is usually called as a keybind by the wm
+# (see ~/.config/i3/config file OR ~/.local/src/chadwm/config.def.h)
 
 ENC_VAULT="$HOME/area/mind_matrix"
 MOUNT_P="$HOME/area/mounts/mind_matrix"
 
 # Mount encrypted dir
-gocryptfs -extpass "zenity --password" "$ENC_VAULT" "$MOUNT_P"
+gocryptfs -extpass "zenity --password" "$ENC_VAULT" "$MOUNT_P" || dunstify -u critical -a "obsdstart" "😥 Couldn't decrypt vault..." && exit 1
 
 # Start obsidian and wait for it to close
 obsidian &
