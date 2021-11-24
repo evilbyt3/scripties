2572fabc8ba456d44f7370178ab9c40e584ef819
vlagh3 <82247523+vlagh3@users.noreply.github.com>
2021-11-24 22:34:25 +0200

    random

--- system/setbg.sh
+++ system/setbg.sh
@@ -6,7 +6,7 @@
 # Description
 # -----------
 # 	This script does the following:
-#		Run by itself, set the wallpaper (at X start).
+#	        Run by itself, set the wallpaper (at X start).
 #		If given a file, set that as the new wallpaper.
 #		If given a directory, choose random file in it.
 #		If wal is installed, also generates a colorscheme.
@@ -17,9 +17,9 @@ bgloc="${XDG_DATA_HOME:-$HOME/.local/share/}/bg"
 
 trueloc="$(readlink -f "$1")" &&
 case "$(file --mime-type -b "$trueloc")" in
-	image/* ) ln -sf "$(readlink -f "$1")" "$bgloc" && dunstify -a "setbg.sh" -u low "⛰<fe0f> Changing wallpaper..." ;;
-	inode/directory ) ln -sf "$(find "$trueloc" -iregex '.*.\(jpg\|jpeg\|png\|gif\)' -type f | shuf -n 1)" "$bgloc" && dunstify -a "setbg.sh" -u low "⛰<fe0f> Choosing random wallpaper..." ;;
-	*) notify-send "Error" "Not a valid image." ; exit 1;;
+  image/* ) ln -sf "$(readlink -f "$1")" "$bgloc" && dunstify -a "setbg.sh" -u low "⛰<fe0f> Changing wallpaper..." ;;
+  inode/directory ) ln -sf "$(find "$trueloc" -iregex '.*.\(jpg\|jpeg\|png\|gif\)' -type f | shuf -n 1)" "$bgloc" && dunstify -a "setbg.sh" -u low "⛰<fe0f> Choosing random wallpaper..." ;;
+  *) notify-send "Error" "Not a valid image." ; exit 1;;
 esac
 
 # If pywal is installed, use it.
@@ -27,7 +27,7 @@ esac
 # 	wal -i "$trueloc" -o "${XDG_CONFIG_HOME:-$HOME/.config}/wal/postrun" >/dev/null 2>&1 &&
 # 	pidof dwm >/dev/null && xdotool key super+F12
 
-xwallpaper --zoom "$bgloc" 
+xwallpaper --stretch "$bgloc" 
 
 # If betterlockscreen installed, use it
 command -v betterlockscreen >/dev/null 2>&1 && betterlockscreen -u "$bgloc"
