427ef1c46f30b4712804e9918ef27526b3f1ca5a
vlaghe <82247523+vlagh3@users.noreply.github.com>
2022-04-05 14:40:35 +0300

    updated mindbak.sh script to only add commits

--- cron/mindbak.sh
+++ cron/mindbak.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-VAULT="$HOME/admin/mind_matrix"
+VAULT="$HOME/area/mind_matrix"
 GITSERV="192.168.1.188"
 
 function panic {
-    echo "[!] $1" && dunstify -a "mindbak" -u critical "❕ Cannot reach git server @ $GITSERV" && exit
+    echo "[!] $1" && dunstify -a "mindbak" -u critical "❕$1 @ $GITSERV" && exit
 }
 
 cd "$VAULT" && dunstify -a "mindbak" -u low "🌩  Backing up vault to local git..."
 CHANGES_EXIST="$(git status --porcelain | wc -l)"
-echo $CHANGES_EXIST
+#echo $CHANGES_EXIST
 [[ -z "$CHANGES_EXIST" ]] && exit 0
 
 # Check if local git server is reachable
-ping -c 1 $GITSERV &> /dev/null || panic "Can't reach git server"
+#ping -c 1 $GITSERV &> /dev/null || panic "Can't reach git server"
 
-git pull home master && git add . && git commit -q -m "Last Sync: $(date +"%Y-%m-%d %H:%M:%S")" &&
-    git push -q home master && dunstify -a "mindbak" -u low "Finished backing up mind"
+git add . && git commit -q -m "Last Sync: $(date +"%Y-%m-%d %H:%M:%S")"
+#git pull origin master && git add . && git commit -q -m "Last Sync: $(date +"%Y-%m-%d %H:%M:%S")" # &&
+    #git push -q bak master && dunstify -a "mindbak" -u low "Finished backing up mind"
 
 
