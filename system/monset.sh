1da08c8984ed7e9b5545ff5f66d81c98e8d70902
vlaghe <82247523+vlagh3@users.noreply.github.com>
2022-05-23 00:05:00 +0300

    changed 2nd monitor to 3440x1440 & refresh-rate of 165

--- system/monset.sh
+++ system/monset.sh
@@ -2,17 +2,19 @@
 # A simple and nasty bash script to 
 # quickly switch between monitors
 
+# TODO: retrieve max res & refresh rate
+
 screens=$(xrandr | grep " connected " | awk '{print $1}') && read disp1 disp2 <<< $(echo $screens)
 
 case $(echo -e "$screens\nBoth" | rofi -dmenu) in
 	Both)
-		xrandr --output $disp1 --mode 1920x1080 --right-of $disp2 && poly-launch.sh;;
+                xrandr --output $disp2 --mode 3440x1440 --rate 165 --primary --left-of $disp1 && poly-launch.sh;;
 	$disp1)
 		xrandr --output "$disp2" --off
 		xrandr --output "$disp1" --mode 1920x1080 --primary;;
 	$disp2)
 		xrandr --output "$disp1" --off
-		xrandr --output "$disp2" --mode 1920x1080 --primary;;
+		xrandr --output "$disp2" --mode 3440x1440 --rate 165 --primary;;
 	*)
 		echo "Invalid option" && exit 1;;
 esac
