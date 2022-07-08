f42b2ab94b30e830fe572c96c40f0330a877b309
vlaghe <vlaghe@protonmail.com>
2022-07-08 20:56:55 +0300

    fixed reading disp1 & disp2 + updated with the new statusbar

--- system/monset.sh
+++ system/monset.sh
@@ -2,19 +2,17 @@
 # A simple and nasty bash script to 
 # quickly switch between monitors
 
-# TODO: retrieve max res & refresh rate
-
-screens=$(xrandr | grep " connected " | awk '{print $1}') && read disp1 disp2 <<< $(echo $screens)
+screens=$(xrandr | grep " connected " | awk '{print $1}') && read disp1 disp2 <<< $(echo $screens | tr '\n' ' ')
 
 case $(echo -e "$screens\nBoth" | rofi -dmenu) in
 	Both)
-                xrandr --output $disp2 --mode 3440x1440 --rate 165 --primary --left-of $disp1 && poly-launch.sh;;
+		xrandr --output $disp2 --mode 3440x1440 --left-of $disp1 && setbg.sh;;
 	$disp1)
 		xrandr --output "$disp2" --off
 		xrandr --output "$disp1" --mode 1920x1080 --primary;;
 	$disp2)
 		xrandr --output "$disp1" --off
-		xrandr --output "$disp2" --mode 3440x1440 --rate 165 --primary;;
+		xrandr --output "$disp2" --mode 1920x1080 --primary;;
 	*)
 		echo "Invalid option" && exit 1;;
 esac
